c64a8f5555fd00d38b6bfb83ab420bde72e0816e
Billy Wang <yunruiwa@usc.edu>
2022-02-19 13:04:55 -0800

    added animation experiments

---
+++ Assets/Experimental/Animation/PlayDefaultAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Animancer;
+using UnityEngine;
+
+namespace Experimental.Animation
+{
+    [SelectionBase]
+    [AddComponentMenu("Experimental/" + nameof(PlayDefaultAnimation) + " (Experimental)")]
+    public class PlayDefaultAnimation : MonoBehaviour
+    {
+        [SerializeField] private AnimancerComponent _animancer;
+        [SerializeField] private ClipTransition _defaultClip;
+        
+        private void Start()
+        {
+            _animancer.Play(_defaultClip);
+        }
+    }
+}
---
+++ Assets/Experimental/Animation/PlayTimelineOnSpace.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Playables;
+
+namespace Experimental.Animation
+{
+    public class PlayTimelineOnSpace : MonoBehaviour
+    {
+        private ExperimentControls _controls;
+        private PlayableDirector _director;
+        
+        private void Awake()
+        {
+            _controls = new ExperimentControls();
+            _controls.Standard.Enable();
+        }
+
+        private void Start()
+        {
+            _controls.Standard.KbdSpace.performed += HandleSpaceClicked;
+        }
+
+        private void HandleSpaceClicked(InputAction.CallbackContext ctx)
+        {
+            if (ctx.performed)
+            {
+                PlayTimeline();
+            }
+        }
+        
+        private void PlayTimeline()
+        {
+            Debug.Log("[Experimental.Animation] Played timeline");
+            _director.Play();
+        }
+
+        private void OnDestroy()
+        {
+            _controls.Standard.KbdSpace.performed -= HandleSpaceClicked;
+        }
+    }
+}
