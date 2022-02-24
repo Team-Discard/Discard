357d607566bc5afe63d5b96c3d97b1ef00ac18a7
Billy Wang <yunruiwa@usc.edu>
2022-02-24 11:22:30 -0800

    player movement is now a lot smoother when walking up stairs

--- Assets/Scripts/CameraBoom.cs
+++ Assets/Scripts/CameraBoom.cs
@@ -9,13 +9,43 @@ public class CameraBoom : MonoBehaviour
     [SerializeField] private Transform _cameraTransform;
     [SerializeField] private Transform _cameraPivot;
 
+    [SerializeField] private bool _useSpring;
+    private Vector3 _effectivePivotPos;
+    private Vector3 _moveToIdealPivotVelocity;
+
+    private void Awake()
+    {
+        _effectivePivotPos = _cameraPivot.transform.position;
+        _moveToIdealPivotVelocity = Vector3.zero;
+    }
+
     private void Update()
     {
         Debug.Assert(_cameraTransform != null);
         Debug.Assert(_cameraPivot != null);
 
-        var targetPos = _cameraPivot.position - _cameraPivot.forward * _maxLength;
-        _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, targetPos, 0.8f);
-        _cameraTransform.LookAt(_cameraPivot.transform.position);
+        if (_useSpring)
+        {
+            _effectivePivotPos =
+                Vector3.SmoothDamp(_effectivePivotPos, _cameraPivot.transform.position, ref _moveToIdealPivotVelocity,
+                    0.1f);
+        }
+        else
+        {
+            _effectivePivotPos = _cameraPivot.transform.position;
+        }
+        _cameraTransform.position = GetIdealCameraPos();
+        _cameraTransform.LookAt(_effectivePivotPos);
+    }
+
+    private Vector3 GetIdealCameraPos()
+    {
+        return _effectivePivotPos - _cameraPivot.transform.forward * _maxLength;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_effectivePivotPos, 0.5f);
     }
 }
---
+++ Assets/Scripts/Unstable/Entities/PlayerMovementSmoother.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Unstable.Entities
+{
+    public class PlayerMovementSmoother : MonoBehaviour
+    {
+        [SerializeField] private Transform _rootTransform;
+        private Vector3 _effectivePosition;
+        private Vector3 _moveToIdealVelocity;
+        
+        private void Awake()
+        {
+            _moveToIdealVelocity = Vector3.zero;
+            _effectivePosition = _rootTransform.position;
+        }
+
+        private void LateUpdate()
+        {
+            var idealPos = _rootTransform.transform.position;
+            _effectivePosition = Vector3.SmoothDamp(_effectivePosition, idealPos, ref _moveToIdealVelocity, 0.05f);
+            transform.position = _effectivePosition;
+        }
+    }
+}
