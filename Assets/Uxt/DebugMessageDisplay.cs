fa272ada7a92c014737fd34ad8e3935845ba0267
Billy Wang <yunruiwa@usc.edu>
2022-03-07 19:25:12 -0800

    added support for on screen debug messages

---
+++ Assets/Uxt/DebugMessageDisplay.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Uxt
+{
+    public class DebugMessageDisplay : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _textBoxPrefab;
+        [SerializeField] private RectTransform _textBoxRoot;
+        private List<TMP_Text> _textBoxes;
+
+        private void Awake()
+        {
+            _textBoxes = new List<TMP_Text>();
+        }
+
+        private void Update()
+        {
+            var messages = DebugMessageManager.Messages;
+
+            if (_textBoxes.Count > messages.Count)
+            {
+                for (var i = messages.Count; i < _textBoxes.Count; ++i)
+                {
+                    Destroy(_textBoxes[i].gameObject);
+                }
+
+                _textBoxes.RemoveRange(messages.Count, _textBoxes.Count - messages.Count);
+            }
+
+            while (_textBoxes.Count < messages.Count)
+            {
+                _textBoxes.Add(Instantiate(_textBoxPrefab, _textBoxRoot));
+            }
+
+            Debug.Assert(_textBoxes.Count == messages.Count);
+            for (var i = 0; i < messages.Count; ++i)
+            {
+                var textBox = _textBoxes[i];
+                var message = messages[i];
+
+                textBox.color = message.Color;
+                textBox.text = message.Text;
+            }
+
+            DebugMessageManager.Tick(Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Uxt/DebugMessageManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Uxt
+{
+    public static class DebugMessageManager
+    {
+        public class Message
+        {
+            public int Id { get; set; }
+            public float TimeRemaining { get; set; }
+            public string Text { get; set; }
+            public Color Color { get; set; }
+        }
+
+        private static int _nextId;
+        private static Dictionary<int, Message> _indexedMessages;
+        private static List<Message> _messages;
+        private static List<Message> _expiredMessageBuffer;
+
+        public static IReadOnlyList<Message> Messages => _messages;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void StaticReset()
+        {
+            _nextId = 0;
+            _indexedMessages = new Dictionary<int, Message>();
+            _messages = new List<Message>();
+            _expiredMessageBuffer = new List<Message>();
+        }
+
+        public static int AllocateId() => _nextId++;
+
+        public static void AddOnScreen(string text) => AddOnScreen(text, -1, Color.red);
+
+        public static void AddOnScreen(string text, int id, Color color, float duration = 1.5f)
+        {
+            if (id < 0)
+            {
+                var message = new Message()
+                {
+                    TimeRemaining = duration,
+                    Text = text,
+                    Color = color,
+                    Id = -1
+                };
+                _messages.Add(message);
+            }
+            else
+            {
+                if (_indexedMessages.TryGetValue(id, out var message))
+                {
+                    message.TimeRemaining = duration;
+                    message.Text = text;
+                    message.Color = color;
+                }
+                else
+                {
+                    message = new Message
+                    {
+                        TimeRemaining = duration,
+                        Text = text,
+                        Color = color,
+                        Id = id
+                    };
+                    _indexedMessages.Add(id, message);
+                    _messages.Add(message);
+                }
+            }
+        }
+
+        public static void Tick(float deltaTime)
+        {
+            _expiredMessageBuffer.Clear();
+            foreach (var message in _messages)
+            {
+                message.TimeRemaining -= deltaTime;
+                if (message.TimeRemaining < 0.0f)
+                {
+                    _expiredMessageBuffer.Add(message);
+                }
+            }
+            _messages.RemoveAll(m => m.TimeRemaining < 0.0f);
+            foreach (var expired in _expiredMessageBuffer)
+            {
+                _indexedMessages.Remove(expired.Id);
+            }
+        }
+    }
+}
