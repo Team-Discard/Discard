ee619b03a0cc927eeb8848ae23aaa702f0d721a6
Billy Wang <yunruiwa@usc.edu>
2022-03-09 20:23:26 -0800

    fixed formatting

--- Assets/Uxt/Debugging/ReflectiveHandler.cs
+++ Assets/Uxt/Debugging/ReflectiveHandler.cs
@@ -7,107 +7,107 @@ using UnityEngine;
 namespace Uxt.Debugging
 {
     /// <summary>
-        /// A handler that reacts to commands by looking at the method overloads
-        /// of a class's member functions and determine the best function to call.
-        /// </summary>
-        public class ReflectiveHandler : ICommandHandler
+    /// A handler that reacts to commands by looking at the method overloads
+    /// of a class's member functions and determine the best function to call.
+    /// </summary>
+    public class ReflectiveHandler : ICommandHandler
+    {
+        private const BindingFlags MethodBindingFlags =
+            BindingFlags.Instance |
+            BindingFlags.Static |
+            BindingFlags.Public |
+            BindingFlags.NonPublic;
+
+        private readonly object _instance;
+        private readonly List<MethodInfo> _overloads;
+
+        public ReflectiveHandler(object instance)
         {
-            private const BindingFlags MethodBindingFlags =
-                BindingFlags.Instance |
-                BindingFlags.Static |
-                BindingFlags.Public |
-                BindingFlags.NonPublic;
+            _instance = instance;
+            _overloads = _instance
+                .GetType()
+                .GetMethods(MethodBindingFlags)
+                .Where(m => m.GetCustomAttribute<DebugCommandAttribute>() != null)
+                .ToList();
+        }
 
-            private readonly object _instance;
-            private readonly List<MethodInfo> _overloads;
-
-            public ReflectiveHandler(object instance)
+        public void HandleCommand(string command)
+        {
+            var paramStrings = GetParameterStrings(command);
+            var method = FindBestOverload(paramStrings, out var parsedParams);
+            if (method == null)
             {
-                _instance = instance;
-                _overloads = _instance
-                    .GetType()
-                    .GetMethods(MethodBindingFlags)
-                    .Where(m => m.GetCustomAttribute<DebugCommandAttribute>() != null)
-                    .ToList();
+                DebugConsole.PrintMessage("No handler found for parameters");
+                return;
             }
 
-            public void HandleCommand(string command)
+            method.Invoke(method.IsStatic ? null : _instance, parsedParams);
+        }
+
+        private static List<string> GetParameterStrings(string command)
+        {
+            var parameters = command.Split().ToList();
+            Debug.Assert(parameters.Count >= 1);
+            parameters.RemoveAt(0);
+            return parameters;
+        }
+
+        private MethodInfo FindBestOverload(List<string> paramStrings, out object[] parsedParams)
+        {
+            foreach (var overload in _overloads)
             {
-                var paramStrings = GetParameterStrings(command);
-                var method = FindBestOverload(paramStrings, out var parsedParams);
-                if (method == null)
+                if (OverloadMatchesParams(overload, paramStrings, out parsedParams))
                 {
-                    DebugConsole.PrintMessage("No handler found for parameters");
-                    return;
+                    return overload;
                 }
-
-                method.Invoke(method.IsStatic ? null : _instance, parsedParams);
             }
 
-            private static List<string> GetParameterStrings(string command)
+            parsedParams = default;
+            return null;
+        }
+
+        private static bool OverloadMatchesParams(
+            MethodInfo overload,
+            List<string> paramStrings,
+            out object[] parsedParams)
+        {
+            parsedParams = default;
+            var paramInfos = overload.GetParameters();
+            if (paramStrings.Count != paramInfos.Length)
             {
-                var parameters = command.Split().ToList();
-                Debug.Assert(parameters.Count >= 1);
-                parameters.RemoveAt(0);
-                return parameters;
+                return false;
             }
 
-            private MethodInfo FindBestOverload(List<string> paramStrings, out object[] parsedParams)
+            parsedParams = new object[paramInfos.Length];
+
+            for (var i = 0; i < paramInfos.Length; ++i)
             {
-                foreach (var overload in _overloads)
+                var paramInfo = paramInfos[i];
+                var type = paramInfo.ParameterType;
+                var paramStr = paramStrings[i];
+                if (type == typeof(int) && int.TryParse(paramStr, out var intVal))
                 {
-                    if (OverloadMatchesParams(overload, paramStrings, out parsedParams))
-                    {
-                        return overload;
-                    }
+                    parsedParams[i] = intVal;
                 }
-
-                parsedParams = default;
-                return null;
-            }
-
-            private static bool OverloadMatchesParams(
-                MethodInfo overload,
-                List<string> paramStrings,
-                out object[] parsedParams)
-            {
-                parsedParams = default;
-                var paramInfos = overload.GetParameters();
-                if (paramStrings.Count != paramInfos.Length)
+                else if (type == typeof(float) && float.TryParse(paramStr, out var floatVal))
+                {
+                    parsedParams[i] = floatVal;
+                }
+                else if (type.IsEnum && Enum.TryParse(type, paramStr, true, out var enumVal))
+                {
+                    parsedParams[i] = enumVal;
+                }
+                else if (type == typeof(string))
+                {
+                    parsedParams[i] = paramStr;
+                }
+                else
                 {
                     return false;
                 }
-
-                parsedParams = new object[paramInfos.Length];
-
-                for (var i = 0; i < paramInfos.Length; ++i)
-                {
-                    var paramInfo = paramInfos[i];
-                    var type = paramInfo.ParameterType;
-                    var paramStr = paramStrings[i];
-                    if (type == typeof(int) && int.TryParse(paramStr, out var intVal))
-                    {
-                        parsedParams[i] = intVal;
-                    }
-                    else if (type == typeof(float) && float.TryParse(paramStr, out var floatVal))
-                    {
-                        parsedParams[i] = floatVal;
-                    }
-                    else if (type.IsEnum && Enum.TryParse(type, paramStr, true, out var enumVal))
-                    {
-                        parsedParams[i] = enumVal;
-                    }
-                    else if (type == typeof(string))
-                    {
-                        parsedParams[i] = paramStr;
-                    }
-                    else
-                    {
-                        return false;
-                    }
-                }
-
-                return true;
             }
+
+            return true;
         }
+    }
 }
