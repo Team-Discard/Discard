4c4b0848e841864efea0ede611908426d9d4dfec
Leyu Xu <leyuxu@usc.edu>
2022-03-22 22:57:26 -0700

    start building interaction system

---
+++ Assets/Scripts/InteractionSystem/InteractionEventSystem.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace InteractionSystem
+{
+    public static class InteractionEventSystem
+    {
+        private static bool _IsInteracting = false;
+        
+        // on start interaction event
+        private static event Action<int> OnStartInteraction;
+        public static void SubscribeToOnStartInteraction(Action<int> subscriber) => OnStartInteraction += subscriber;
+        public static void UnSubscribeToOnStartInteraction(Action<int> subscriber) => OnStartInteraction -= subscriber;
+        public static void TriggerOnStartInteraction(int interactableObjId)
+        {
+            // if we are already interacting with something, do not trigger more interactions
+            if (_IsInteracting)
+            {
+                return;
+            }
+            
+            // else set interacting to true and trigger interaction event
+            _IsInteracting = true;
+            OnStartInteraction?.Invoke(interactableObjId);
+        }
+
+        // on end interaction event
+        private static event Action<int> OnEndInteraction;
+        public static void SubscribeToOnEndInteraction(Action<int> subscriber) => OnEndInteraction += subscriber;
+        public static void UnSubscribeToOnEndInteraction(Action<int> subscriber) => OnEndInteraction -= subscriber;
+
+        public static void TriggerOnEndInteraction(int interactableObjId)
+        {
+            _IsInteracting = false;
+            OnEndInteraction?.Invoke(interactableObjId);
+        }
+    }
+}
