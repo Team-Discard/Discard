d66b6eea8bec1829507919237d62c6cec8672c56
Billy Wang <yunruiwa@usc.edu>
2022-04-04 13:25:44 -0700

    added script to check lfs locked files

---
+++ Assets/Uxt/VersionControl/Git/LfsLock.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace Uxt.VersionControl.Git
+{
+    public static class LfsLock
+    {
+        public static async Task<List<string>> GetLockedFilesGuid()
+        {
+            try
+            {
+                var processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "C:\\Program Files\\Git\\cmd\\git-lfs.exe",
+                    CreateNoWindow = true,
+                    WorkingDirectory = Application.dataPath,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    Arguments = "locks --json"
+                };
+                var git = Process.Start(processStartInfo);
+                Debug.Assert(git != null);
+
+                await Task.Run(() => { git.WaitForExit(); });
+
+                var output = await git.StandardOutput.ReadToEndAsync();
+                Debug.Log(output);
+                
+                return null;
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                throw;
+            }
+        }
+
+        [MenuItem("Git/Show Locked Files")]
+        public static void ReadFiles()
+        {
+            GetLockedFilesGuid();
+        }
+    }
+}
