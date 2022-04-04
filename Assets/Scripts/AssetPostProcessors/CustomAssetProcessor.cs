871c80de719afe2ea6bb4dcdfbb63c436d688b0c
Billy Wang <yunruiwa@usc.edu>
2022-04-04 12:37:34 -0700

    added asset post processors for warning on readonly files

---
+++ Assets/Scripts/AssetPostProcessors/CustomAssetProcessor.cs
@@ -0,0 +1,34 @@
+﻿#if UNITY_EDITOR
+
+using System.Collections.Generic;
+using System.IO;
+
+namespace AssetPostProcessors
+{
+    public class CustomAssetProcessor : UnityEditor.AssetModificationProcessor
+    {
+        public static string[] OnWillSaveAssets(string[] paths)
+        {
+            var pathsToSave = new List<string>();
+
+            for (int i = 0; i < paths.Length; ++i)
+            {
+                var info = new FileInfo(paths[i]);
+                if (info.IsReadOnly)
+                {
+                    UnityEditor.EditorUtility.DisplayDialog("Can not save.",
+                        "Sorry, but '" + paths[i] + "' is Read-Only! Please Save as another name.",
+                        "Ok");
+                }
+                else
+                {
+                    pathsToSave.Add(paths[i]);
+                }
+            }
+
+            return pathsToSave.ToArray();
+        }
+    }
+}
+
+#endif
