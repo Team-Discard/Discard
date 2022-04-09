c403f385ae0e67db45f6e2b21a8c948f20e06e59
Billy Wang <yunruiwa@usc.edu>
2022-04-08 21:29:35 -0700

    fixed player controller motion in dialogues

--- Assets/Scripts/PlayerSystem/StandardPlayerController.cs
+++ Assets/Scripts/PlayerSystem/StandardPlayerController.cs
@@ -33,7 +33,12 @@ namespace PlayerSystem
         public void Tick(float deltaTime)
         {
             // very hacky
-            if (GameRuleManager.IsRuleEnforced(GameRule.PlayerCannotMove)) return;
+            if (GameRuleManager.IsRuleEnforced(GameRule.PlayerCannotMove))
+            {
+                _pawn.SetTranslation(Translation.Identity);
+                _pawn.SetRotation(Rotation.Identity);
+                return;
+            }
 
             var rotationFrame = Rotation.Identity;
             var translationFrame = Translation.Identity;
